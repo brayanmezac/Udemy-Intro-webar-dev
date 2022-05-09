97e754be7731699a122413f67066c398836be2c2
Nathan Barnes <nbarnes@azahner.com>
2022-05-09 12:55:38 -0500

    update material

--- main.js
+++ main.js
@@ -1,71 +1,44 @@
-import * as THREE from '../../libs/three.js-r132/build/three.module.js';
-import {ARButton} from '../../libs/three.js-r132/examples/jsm/webxr/ARButton.js';
+const THREE = window.MINDAR.IMAGE.THREE;
 
-document.addEventListener('DOMContentLoaded', () => {
-  const initialize = async() => {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera();
-
-    const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
-    scene.add(light);
-
-    const reticleGeometry = new THREE.RingGeometry( 0.15, 0.2, 32 ).rotateX(- Math.PI / 2);
-    const reticleMaterial = new THREE.MeshBasicMaterial(); 
-    const reticle = new THREE.Mesh(reticleGeometry, reticleMaterial);
-    reticle.matrixAutoUpdate = false;
-    reticle.visible = false;
-    scene.add(reticle);
-
-    const renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.xr.enabled = true;
-
-    const arButton = ARButton.createButton(renderer, {requiredFeatures: ['hit-test'], optionalFeatures: ['dom-overlay'], domOverlay: {root: document.body}});
-    document.body.appendChild(renderer.domElement);
-    document.body.appendChild(arButton);
-
-    const controller = renderer.xr.getController(0);
-    scene.add(controller);
-    controller.addEventListener('select', () => {
-      const geometry = new THREE.BoxGeometry(0.06, 0.06, 0.06); 
-      const material = new THREE.MeshBasicMaterial({ color: 0xffffff * Math.random()});
-      const mesh = new THREE.Mesh(geometry, material);
-      mesh.position.setFromMatrixPosition(reticle.matrix);
-      mesh.scale.y = Math.random() * 2 + 1;
-      scene.add(mesh);
+document.addEventListener("DOMContentLoaded", () => {
+  const start = async () => {
+    // initialize MindAR
+    const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+      container: document.body,
+      // imageTargetSrc: '../../assets/targets/course-banner.mind',
+      imageTargetSrc: "../../assets/targets/Side-.mind",
+      maxTrack: 2,
     });
+    const { renderer, scene, camera } = mindarThree;
 
-    renderer.xr.addEventListener("sessionstart", async (e) => {
-      const session = renderer.xr.getSession();
-      const viewerReferenceSpace = await session.requestReferenceSpace("viewer");
-      const hitTestSource = await session.requestHitTestSource({space: viewerReferenceSpace});
-
-      renderer.setAnimationLoop((timestamp, frame) => {
-	if (!frame) return;
-
-	const hitTestResults = frame.getHitTestResults(hitTestSource);
-
-	if (hitTestResults.length) {
-	  const hit = hitTestResults[0];
-	  const referenceSpace = renderer.xr.getReferenceSpace(); // ARButton requested 'local' reference space
-	  const hitPose = hit.getPose(referenceSpace);
-
-	  reticle.visible = true;
-	  reticle.matrix.fromArray(hitPose.transform.matrix);
-	} else {
-	  reticle.visible = false;
-	}
-
-	renderer.render(scene, camera);
-      });
+    // create AR object
+    const geometry1 = new THREE.PlaneGeometry(1.5, 1.5);
+    const material1 = new THREE.MeshBasicMaterial({
+      color: 0x00ffff,
+      transparent: true,
+      opacity: 0.5,
     });
+    const plane1 = new THREE.Mesh(geometry1, material1);
 
-    renderer.xr.addEventListener("sessionend", () => {
-      console.log("session end");
+    const geometry2 = new THREE.PlaneGeometry(1.5, 1.5);
+    const material2 = new THREE.MeshBasicMaterial({
+      color: 0xebf1f5,
+      transparent: false,
+      opacity: 0.5,
     });
+    const plane2 = new THREE.Mesh(geometry2, material2);
 
-  }
+    // create anchor
+    const anchorside1 = mindarThree.addAnchor(0);
+    anchorside1.group.add(plane1);
+    const anchorside2 = mindarThree.addAnchor(1);
+    anchorside2.group.add(plane2);
 
-  initialize();
+    // start AR
+    await mindarThree.start();
+    renderer.setAnimationLoop(() => {
+      renderer.render(scene, camera);
+    });
+  };
+  start();
 });
